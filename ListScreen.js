481635d16b4d29bf62aeceef24f627c644126930
kjng <pianistkevinjang@gmail.com>
2017-02-19 10:43:05 -0800

    Display Harry Potter movies in ListView

--- ListScreen.js
+++ ListScreen.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
-import { TouchableHighlight, StyleSheet, Text, View, ListView } from 'react-native';
+import { TouchableHighlight, StyleSheet, Text, View, Image, ListView } from 'react-native';
+import API from './api.js';
 
 export default class ListScreen extends Component {
   constructor(props) {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows(['Row 1', 'Row 2', 'Row 3', 'Row 4'])
+      dataSource: ds.cloneWithRows([])
     };
+    API.search('harry potter').then(data => {
+      this.setState({dataSource: ds.cloneWithRows(data)});
+    });
   }
 
   render() {
     return (
-      <ListView style={styles.container}
+      <ListView
+        style={styles.container}
+        enableEmptySections={true}
         dataSource={this.state.dataSource}
-        renderRow={(rowData) => (<Text>{rowData}</Text>)}
+        renderRow={(rowData) => {
+          console.log(rowData);
+          return (
+            <View style={styles.row}>
+              <View style={{flex: 3, marginTop: 5, marginBottom: 5}}>
+                <Image style={styles.image} source={{uri: rowData.Poster}} />
+              </View>
+              <View style={{flex: 10, padding: 10, justifyContent: 'center'}}>
+                <Text style={styles.movieTitle}>{rowData.Title} ({rowData.Year})</Text>
+              </View>
+              <View style={{flex: 1, justifyContent: 'center'}}>
+                <Text style={styles.arrow}>></Text>
+              </View>
+            </View>)
+        }}
         renderSeparator={(sectionID, rowID, adjacentRowHighlighted) => (
           <View key={rowID} style={{height: 1, backgroundColor: 'black'}} />
         )}
@@ -28,5 +48,18 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     paddingTop: 65
+  },
+  row: {
+    flexDirection: 'row',
+    height: 100
+  },
+  image: {
+    height: 90
+  },
+  movieTitle: {
+    fontSize: 15
+  },
+  arrow: {
+    fontSize: 20
   }
 });
