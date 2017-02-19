b55709c0c7360c37d7052412612121ed5fca324b
kjng <pianistkevinjang@gmail.com>
2017-02-19 10:23:59 -0800

    Create movie api functions

---
+++ api.js
@@ -0,0 +1,15 @@
+const rootURL = 'https://www.omdbapi.com';
+
+exports.search = (query) => {
+  let url = `${rootURL}?s=${query}`;
+  console.log(url);
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => json.Search);
+};
+
+exports.view = (id) => {
+  let url = `${rootURL}?i=${id}&plot=short&r=json`;
+  return fetch(url)
+    .then(res => res.json());
+};
